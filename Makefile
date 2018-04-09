09e579ac9a89d3d5d7b3e79cd0d3e1b558d68fcf
Yash Verma <verma.yash8@gmail.com>
2018-04-09 20:09:02 +0530

    Got the output in e-315

--- Makefile
+++ Makefile
@@ -2,19 +2,24 @@ CUDA_HOME=/usr/local/cuda-8.0
 NVCC=$(CUDA_HOME)/bin/nvcc
 
 CUDA_INCLUDEPATH=$(CUDA_HOME)/include
-NVCC_OPTS=-O3 -arch=sm_20 -Xcompiler -m64 -Wno-deprecated-gpu-targets
-GCC_OPTS=-O3 -m64
+NVCC_OPTS=-O3 -arch=sm_20 -Xcompiler -m64 -Wno-deprecated-gpu-targets -std=c++11
+GCC_OPTS=-O3 -m64 -std=c++11
 
-major: start.o ga_helper.o Makefile
-	$(NVCC) -o major start.o ga_helper.o $(NVCC_OPTS)
-
-start.o: start.cpp ga_helper.h
-	# Doing only for now since all the code is in one file.
-	$(NVCC) -c start.cpp $(NVCC_OPTS)
-	# g++ -c start.cpp $(GCC_OPTS) -I $(CUDA_INCLUDEPATH)
-
-ga_helper.o: ga_helper.h ga_helper.cu
-	$(NVCC) -c ga_helper.cu $(NVCC_OPTS)
+major: start.cpp
+	g++ -std=c++11 -O3 -m64 -o major start.cpp
+#
+#
+#
+#major: start.o ga_helper.o Makefile
+#	$(NVCC) -o major start.o ga_helper.o $(NVCC_OPTS)
+#
+#start.o: start.cpp ga_helper.h
+#	# Doing only for now since all the code is in one file.
+#	$(NVCC) -c start.cpp $(NVCC_OPTS)
+#	# g++ -c start.cpp $(GCC_OPTS) -I $(CUDA_INCLUDEPATH)
+#
+#ga_helper.o: ga_helper.h ga_helper.cu
+#	$(NVCC) -c ga_helper.cu $(NVCC_OPTS)
 
 clean: 
 	rm -f *.o major
--- start.cpp
+++ start.cpp
@@ -1,50 +1,60 @@
 #include <stdio.h>
 #include <string.h>
-#include <cstdlib>
+//#include <cstdlib>
 #include <time.h>
 #include <algorithm>
+#include <random>
+#include <math.h>
+#include <iostream>
+using namespace std;
+typedef long double genetype;
 
 const int GENOME_LENGTH = 16;
-const int NUMBER_CHROMOSOMES = 1000;
+const int NUMBER_CHROMOSOMES = 100;
 
-const int GENE_MIN = -1;
-const int GENE_MAX = +1;
+const double GENE_MIN = -1;
+const double GENE_MAX = +1;
 
-const int MUTATION_FACTOR = 0.2;
-const int CROSSOVER_RATE = 0.6;
+const float MUTATION_FACTOR = 0.2;
+const float CROSSOVER_RATE = 0.6;
 
-const int NUM_EPOCHS = 10000;
+const int NUM_EPOCHS = 1000;
+
+default_random_engine generator;
+uniform_real_distribution<genetype> geneValueDistribution(GENE_MIN, GENE_MAX);
 
 class Chromosome {
-	double genes[GENOME_LENGTH];
+	genetype genes[GENOME_LENGTH];
 
 public:
 
 	Chromosome() {
 		for (int j = 0; j < GENOME_LENGTH; j++) {
-			genes[j] = (2.0 * GENE_MAX * rand()) / RAND_MAX - GENE_MAX;
+			genes[j] = geneValueDistribution(generator);
 		}
 	}
 
-	Chromosome(double genes[]) {
-		memcpy(this->genes, genes, GENOME_LENGTH * sizeof(double));
+	Chromosome(genetype genes[]) {
+		memcpy(this->genes, genes, GENOME_LENGTH * sizeof(genetype));
 	}
 
-	double* getGenes() {
+	genetype* getGenes() {
 		return genes;
 	}
 
-	double getFitnessValue() {
-		double value = 0.0;
+	genetype getFitnessValue() {
+		genetype value = 0.0;
 		for (int i = 0; i < GENOME_LENGTH; i++) {
 			value += genes[i] * genes[i];
 		}
+//		printf("FITNESS: %e", value);
 		return value;
 	}
 
 	Chromosome crossover(Chromosome b) {
+//		printf("CROSSING OVER...\n");
 		int mid = GENOME_LENGTH / 2;
-		double offspringGenes[GENOME_LENGTH];
+		genetype offspringGenes[GENOME_LENGTH];
 		for (int i = 0; i < GENOME_LENGTH; i++) {
 			if (i <= mid) {
 				offspringGenes[i] = genes[i];
@@ -56,8 +66,9 @@ public:
 	}
 
 	void mutate() {
+//		printf("MUTATING...\n");
 		for (int i = 0; i < GENOME_LENGTH; i++) {
-			double multiplier = (0.2 * rand()) / RAND_MAX - 0.1;
+			genetype multiplier = geneValueDistribution(generator) / 10;
 			genes[i] *= multiplier;
 			if (multiplier < -0.1 || multiplier > 0.1) {
 				printf("Multiplier is wrong. %f", multiplier);
@@ -68,9 +79,9 @@ public:
 	void print() {
 		printf("Chromosome: ");
 		for (int i = 0; i < GENOME_LENGTH; i++) {
-			printf("%lf ", genes[i]);
+			printf("%e ", genes[i]);
 		}
-		printf("\nFitness: %lf\n", getFitnessValue());
+		printf("\nFitness: %e\n", getFitnessValue());
 	}
 
 };
@@ -80,36 +91,50 @@ bool fitnessComparator(Chromosome &a, Chromosome &b) {
 }
 
 void startIteration(Chromosome population[]) {
-	int num_parents = NUMBER_CHROMOSOMES * (1 - CROSSOVER_RATE);
+	int num_parents = (1.0 * NUMBER_CHROMOSOMES * (1 - CROSSOVER_RATE));
 	int num_offsprings = NUMBER_CHROMOSOMES - num_parents;
+
+	uniform_int_distribution<int> parentIndexDistribution(0, num_parents);
 
 	std::sort(population, population + NUMBER_CHROMOSOMES, fitnessComparator);
 
 	int offspringStartIndex = num_parents;
+//	printf("Num_parents:%d , NumChromosomes:%d", num_parents, NUMBER_CHROMOSOMES);
+	int count = 0;
 
 	for (int i = offspringStartIndex; i < NUMBER_CHROMOSOMES; i++) {
-		int fatherIndex = rand() % num_parents;
-		int motherIndex = rand() % num_parents;
+		int fatherIndex = parentIndexDistribution(generator);
+		int motherIndex = parentIndexDistribution(generator);
+
+
+		if(motherIndex == fatherIndex) {
+			count++;
+			continue;
+		}
+
 		Chromosome male = population[fatherIndex];
 		Chromosome female = population[motherIndex];
 		Chromosome offspring = male.crossover(female);
 
-		if (MUTATION_FACTOR >= ((double) rand()) / RAND_MAX) {
+		if (MUTATION_FACTOR >= abs(geneValueDistribution(generator))) {
 			offspring.mutate();
 		}
 		population[i] = offspring;
 	}
+//	printf("Saved you : %d times\n", count);
 }
 
 Chromosome geneticAlgorithm(Chromosome population[]) {
 	for (int z = 0; z < NUM_EPOCHS; z++) {
+		printf("Iteration:%d\n", z);
 		startIteration(population);
 	}
+	std::sort(population, population + NUMBER_CHROMOSOMES, fitnessComparator);
 	return population[0];
 }
 
 int main() {
-	srand(time(NULL));
+//	srand(time(NULL));
 	Chromosome population[NUMBER_CHROMOSOMES];
 
 	Chromosome best = geneticAlgorithm(population);
