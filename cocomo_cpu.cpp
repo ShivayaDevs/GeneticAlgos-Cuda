cbe6f0fc31d37e2e72d9f70fe58ce712d93a07dd
Yash Verma <verma.yash8@gmail.com>
2018-05-04 12:30:24 +0530

    Cleaned up CPU code

--- cocomo_cpu.cpp
+++ cocomo_cpu.cpp
@@ -19,10 +19,14 @@ const float CROSSOVER_RATE = 0.4;
 
 const int NUM_EPOCHS = 1500;
 
-long double project[20], project_tdev[20];
+const int NUM_PROJECTS = 12;
+
+HighlyPrecise kiloLinesOfCode[NUM_PROJECTS];
+HighlyPrecise actualEfforts[NUM_PROJECTS];
 
 default_random_engine generator;
-uniform_real_distribution<HighlyPrecise> geneValueDistribution(GENE_MIN, GENE_MAX);
+uniform_real_distribution<HighlyPrecise> geneValueDistribution(GENE_MIN,
+		GENE_MAX);
 uniform_real_distribution<HighlyPrecise> geneValueDistribution2(0, 1);
 
 class Chromosome {
@@ -31,10 +35,8 @@ class Chromosome {
 public:
 
 	Chromosome() {
-		cout<<"\n";
 		for (int j = 0; j < GENOME_LENGTH; j++) {
 			genes[j] = geneValueDistribution(generator);
-			cout<<genes[j]<<" ";
 		}
 	}
 
@@ -46,34 +48,18 @@ public:
 		return genes;
 	}
 
-	// long double dev_time(int j){
-	// HighlyPrecise effort = genes[0] * pow((project[j]), genes[1]);
-	// 	HighlyPrecise tdev = genes[2] * pow(effort, genes[3]);
-	// 	return tdev;
-	// }
+	HighlyPrecise getEstimatedEffort(int projectId) {
+		// Effort is estimated by cocomo param (a * KLOC^b).
+		return genes[0] * pow(kiloLinesOfCode[projectId], genes[1]);
+	}
 
-    long double get_effort(int j){
-        HighlyPrecise effort = genes[0] * pow((project[j]), genes[1]);
-        return effort;
-    }
-
-
-	/**
-	 * The current fitness function is: Summation (x * x). It needs to be minimized.
-	 */
 	HighlyPrecise getFitnessValue() {
 		HighlyPrecise value = 0.0;
-
-
-		// for(int j=0; j<20; j++){
-		// 	value += fabs((project_tdev[j] - dev_time(j)))/ (double) project_tdev[j];
-		// }
-		// for (int i = 0; i < GENOME_LENGTH; i++) {
-		// 	value += genes[i] * genes[i];
-		// }
-        for(int j=0; j<12; j++){
-            value += fabs(project_tdev[j] - get_effort(j))*fabs(project_tdev[j] - get_effort(j));
-        }
+		for (int i = 0; i < NUM_PROJECTS; i++) {
+			// TODO: See the possibility of a square here.
+			value += fabs(actualEfforts[i] - getEstimatedEffort(i))
+					* fabs(actualEfforts[i] - getEstimatedEffort(i));
+		}
 		return value;
 	}
 
@@ -91,12 +77,6 @@ public:
 				offspringGenes[i] = b.getGenes()[i];
 			}
 		}
-		Chromosome offspring(offspringGenes);
-//		if(offspring.getFitnessValue() == getFitnessValue() || offspring.getFitnessValue() == b.getFitnessValue()) {
-//			printf("Baccha on maa baap\n");
-//		} else {
-//			printf("NOT on maa baap\n");
-//		}
 		return Chromosome(offspringGenes);
 	}
 
@@ -112,18 +92,14 @@ public:
 	 * Prints the chromosome and its fitness function.
 	 */
 	void print() {
-		// printf("Chromosome: ");
-		// for (int i = 0; i < GENOME_LENGTH; i++) {
-		// 	printf("%Le ", genes[i]);
-		// }
-		// printf("\nFitness: %Le\n", getFitnessValue());
-
-        for (int j=0; j<12; j++){
-            cout<<"Project["<<j<<"] "<<project_tdev[j]<<" : effort = "<<get_effort(j)<<"\n";
-        }
-
-        cout<<"A: "<<genes[0]<<"\n";
-        cout<<"B: "<<genes[1]<<"\n";
+		printf("==> Results:\nProjectId\tKloc\tActualEffort\tEstimatedEffortGA\n");
+		for (int i = 0; i < NUM_PROJECTS; i++) {
+			printf("%d\t%Lf\t%Lf\t%Lf\n", i, kiloLinesOfCode[i], actualEfforts[i],
+					getEstimatedEffort(i));
+		}
+		printf("==> Parameters: ");
+		cout << "A: " << genes[0] << "\n";
+		cout << "B: " << genes[1] << "\n";
 	}
 
 };
@@ -172,53 +148,18 @@ Chromosome geneticAlgorithm(Chromosome population[]) {
 }
 
 int main() {
-	
 
-	// project[0] = 10; project_tdev[0] = 13;
-	// project[1] = 17; project_tdev[1] = 12;
-	// project[2] = 10; project_tdev[2] = 15;
-	// project[3] = 24; project_tdev[3] = 18;
-	// project[4] = 19; project_tdev[4] = 13;
-	// project[5] = 19; project_tdev[5] = 14;
-	// project[6] = 10; project_tdev[6] = 15;
-	// project[7] = 15; project_tdev[7] = 13;
-	// project[8] = 10; project_tdev[8] = 12;
-	// project[9] = 10; project_tdev[9] = 12;
-	// project[10] = 17; project_tdev[10] = 22;
-	// project[11] = 11; project_tdev[11] = 19;
-	// project[12] = 15; project_tdev[12] = 18;
-	// project[13] = 15; project_tdev[13] = 19;
-	// project[14] = 13; project_tdev[14] = 21;
-	// project[15] = 14; project_tdev[15] = 20;
-	// project[16] = 15; project_tdev[16] = 19;
-	// project[17] = 15; project_tdev[17] = 20;
-	// project[18] = 13; project_tdev[18] = 15;
-	// project[19] = 18; project_tdev[19] = 19;
-
-     project[0] = 46.2; project_tdev[0] = 96.0;
-    project[1] = 46.5; project_tdev[1] = 79.0;
-    project[2] = 31.1; project_tdev[2] = 39.6;
-    project[3] = 12.8; project_tdev[3] = 18.9;
-    project[4] = 10.5; project_tdev[4] = 10.3;
-    project[5] = 21.5; project_tdev[5] = 28.5;
-    project[6] = 3.1; project_tdev[6] = 7.0;
-    project[7] = 4.2; project_tdev[7] = 9.0;
-    project[8] = 7.8; project_tdev[8] = 7.3;
-    project[9] = 2.1; project_tdev[9] = 5.0;
-    project[10] = 5.0; project_tdev[10] = 8.4;
-    project[11] = 9.7; project_tdev[11] = 15.6;
-    project[12] = 12.5; project_tdev[12] = 23.9;
-    // project[13] = 15; project_tdev[13] = 19;
-    // project[14] = 13; project_tdev[14] = 21;
-    // project[15] = 14; project_tdev[15] = 20;
-    // project[16] = 15; project_tdev[16] = 19;
-    // project[17] = 15; project_tdev[17] = 20;
-    // project[18] = 13; project_tdev[18] = 15;
-    // project[19] = 18; project_tdev[19] = 19;
-
-
-
-
+	FILE *fp = fopen("cocomo.dataset", "r");
+	if (fp == NULL) {
+		printf("ERROR: Unable to open dataset file.");
+		return 1;
+	}
+	printf("==> Training data:\nKLOC\t\tActualEffort\n");
+	for (int i = 0; i < NUM_PROJECTS && !feof(fp); i++) {
+		fscanf(fp, "%Lf %Lf", &kiloLinesOfCode[i], &actualEfforts[i]);
+		printf("%Lf\t%Lf\n", kiloLinesOfCode[i], actualEfforts[i]);
+	}
+	fclose(fp);
 
 	Chromosome population[NUMBER_CHROMOSOMES];
 	// Start genetic algorithm.
