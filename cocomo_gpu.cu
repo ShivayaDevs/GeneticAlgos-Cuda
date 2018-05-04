14d26c7cb0eb7c15fa6cf6f9751bd99bc1d96cdc
Yash Verma <verma.yash8@gmail.com>
2018-05-04 16:32:12 +0530

    In the process of restructuring to understand

--- cocomo_gpu.cu
+++ cocomo_gpu.cu
@@ -6,8 +6,8 @@
 #include <time.h>
 using namespace std;
 
-#define THREADS_PER_BLOCK 32
-#define NUM_BLOCKS 32
+#define THREADS_PER_BLOCK 10
+#define NUM_BLOCKS 2
 
 typedef double HighlyPrecise;
 
@@ -16,10 +16,10 @@ const int GENE_MAX = 10.0;
 const int GENE_MIN = 0.0;
 
 // TODO: Adjust as per CPU code.
-const float MUTATION_FACTOR = 0.2;
-const float CROSSOVER_RATE = 0.6;
+const float MUTATION_FACTOR = 0.8;
+const float CROSSOVER_RATE = 0.4;
 
-const int NUM_EPOCHS = 10000;
+const int NUM_EPOCHS = 1500;
 
 const int NUM_PROJECTS = 12;
 
@@ -33,6 +33,123 @@ struct Project {
 	HighlyPrecise actualEffort;
 };
 
+Project *h_projects = NULL;
+
+Project* loadDatasetIntoGPU() {
+	Project *d_projects = NULL;
+	// Allocate memory.
+	h_projects = (Project*) malloc(NUM_PROJECTS * sizeof(Project));
+	cudaMalloc((void**) &d_projects, NUM_PROJECTS * sizeof(Project));
+	cudaDeviceSynchronize();
+	checkForCudaErrors();
+
+	FILE *fp = fopen("cocomo.dataset", "r");
+	if (fp == NULL) {
+		printf("ERROR: Unable to open dataset file.");
+		return NULL;
+	}
+//	printf("==> Training data:\nKLOC\t\tActualEffort\n");
+	for (int i = 0; i < NUM_PROJECTS && !feof(fp); i++) {
+		fscanf(fp, "%lf %lf", &h_projects[i].kloc, &h_projects[i].actualEffort);
+//		printf("%lf\t%lf\n", h_projects[i].kloc, h_projects[i].actualEffort);
+	}
+	fclose(fp);
+	cudaMemcpy(d_projects, h_projects, sizeof(Project) * NUM_PROJECTS,
+			cudaMemcpyHostToDevice);
+	cudaDeviceSynchronize();
+	checkForCudaErrors();
+	printf("Input dataset has been copied to GPU successfully.\n");
+	return d_projects;
+}
+
+
+/**
+ * Main Function.
+ */
+int main() {
+	int NUM_TOTAL_THREADS = NUM_BLOCKS * THREADS_PER_BLOCK;
+
+	// Setup the random number generation stream on device.
+	curandState *d_randomStates = NULL;
+	cudaMalloc((void**) &d_randomStates,
+			NUM_TOTAL_THREADS * sizeof(curandState));
+	setupRandomStream<<<NUM_BLOCKS, THREADS_PER_BLOCK>>>(time(NULL),
+			d_randomStates);
+	cudaDeviceSynchronize();
+	checkForCudaErrors();
+	// Load dataset into GPU.
+	Project *d_projects = loadDatasetIntoGPU();
+	// Allocate memory for output on GPU.
+	Chromosome *h_gpuOut = NULL;
+	Chromosome *d_outputPopulation = NULL;
+	h_gpuOut = (Chromosome*) malloc(NUM_BLOCKS * sizeof(Chromosome));
+	cudaMalloc((void**) &d_outputPopulation, NUM_BLOCKS * sizeof(Chromosome));
+	cudaDeviceSynchronize();
+	checkForCudaErrors();
+    // STAGE 1 EXECUTION.
+	geneticAlgorithm<<<NUM_BLOCKS, THREADS_PER_BLOCK>>>(true, NULL,
+			d_randomStates, d_projects, d_outputPopulation);
+
+	cudaMemcpy(h_gpuOut, d_outputPopulation, sizeof(Chromosome) * NUM_BLOCKS,
+			cudaMemcpyDeviceToHost);
+
+	//	stage2
+	Chromosome *d_inputPopulation;
+	cudaMalloc((void**) &d_inputPopulation, sizeof(Chromosome) * NUM_BLOCKS);
+	cudaDeviceSynchronize();
+	checkForCudaErrors();
+
+	for (int i = 0; i < NUM_BLOCKS; i++) {
+//		printf("block %d output:%lf\n", i, h_gpuOut[i].fitnessValue);
+	}
+	cudaMemcpy(d_inputPopulation, h_gpuOut, sizeof(Chromosome) * NUM_BLOCKS,
+			cudaMemcpyHostToDevice);
+
+	geneticAlgorithm<<<1, NUM_BLOCKS>>>(false, d_inputPopulation,
+			d_randomStates, d_dataset, d_outputPopulation);
+
+	cudaMemcpy(h_gpuOut, d_outputPopulation, sizeof(Chromosome) * 1,
+			cudaMemcpyDeviceToHost);
+
+	printf("\n======== GPU Results ========\n");
+	print(h_gpuOut[0].genes, h_projects);
+	printf("\n");
+
+	cudaDeviceSynchronize();
+	checkForCudaErrors();
+
+	// Freeing the resources.
+	cudaFree(d_randomStates);
+	cudaFree(d_outputPopulation);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 __global__ void setupRandomStream(unsigned int seed, curandState* states) {
 	int threadIndex = blockDim.x * blockIdx.x + threadIdx.x;
 	curand_init(seed, threadIndex, 0, &states[threadIndex]);
@@ -45,9 +162,17 @@ __device__ HighlyPrecise getEstimatedEffort(HighlyPrecise genes[],
 
 __device__ HighlyPrecise getFitnessValue(HighlyPrecise genes[],
 		Project projects[]) {
+
+	for (int i = 0; i < NUM_PROJECTS; i++) {
+		printf(
+				"Project %d : Kloc=%lf Actual=%lf Estimated=%lf genes=(%lf, %lf)\n",
+				i, projects[i].kloc, projects[i].actualEffort,
+				getEstimatedEffort(genes, i, projects), genes[0], genes[1]);
+	}
+
 	HighlyPrecise fitnessValue = 0;
 	for (int i = 0; i < NUM_PROJECTS; i++) {
-		HighlyPrecise differenceInEfforts = abs(
+		HighlyPrecise differenceInEfforts = fabs(
 				projects[i].actualEffort
 						- getEstimatedEffort(genes, i, projects));
 		fitnessValue += differenceInEfforts * differenceInEfforts;
@@ -96,14 +221,18 @@ __device__ void printBlockPopulation(Chromosome blockPopulation[]) {
 }
 
 __device__ void initializeBlockPopulation(Chromosome blockPopulation[],
-		curandState* randomState, Projects projects[]) {
+		curandState* randomState, Project projects[]) {
 	HighlyPrecise chromosome[GENOME_LENGTH];
 	for (int i = 0; i < GENOME_LENGTH; i++) {
 		// Changed because 0.0 to 10.0
 		chromosome[i] = curand_uniform(randomState) * GENE_MAX;
 		blockPopulation[threadIdx.x].genes[i] = chromosome[i];
 	}
-	blockPopulation[threadIdx.x].fitnessValue = getFitnessValue(chromosome, projects);
+	blockPopulation[threadIdx.x].fitnessValue = getFitnessValue(chromosome,
+			projects);
+	printf("PI: Fitness=%lf, Chromosome=(%lf %lf)\n",
+			blockPopulation[threadIdx.x].fitnessValue, chromosome[0],
+			chromosome[1]);
 }
 
 __device__ Chromosome crossover(Chromosome blockPopulation[],
@@ -127,13 +256,14 @@ __device__ Chromosome crossover(Chromosome blockPopulation[],
 
 __device__ void mutate(Chromosome *offspring, curandState* randomState) {
 	for (int i = 0; i < GENOME_LENGTH; i++) {
-		HighlyPrecise multiplier = (2.0 * curand_uniform(randomState) - 1) / 10;
+		// XXX: Changed. Multiplier in this is from 0 to 1.
+		HighlyPrecise multiplier = curand_uniform(randomState);
 		offspring->genes[i] *= multiplier;
 	}
 }
 
 __device__ void startIteration(Chromosome blockPopulation[],
-		curandState* randomState, Projects projects[]) {
+		curandState* randomState, Project projects[]) {
 
 	int num_parents = blockDim.x * (1 - CROSSOVER_RATE);
 
@@ -215,91 +345,16 @@ void checkForCudaErrors() {
 	}
 }
 
-Project* loadDatasetIntoGPU() {
-	Project *h_projects = NULL;
-	Project *d_projects = NULL;
-	// Allocate memory.
-	h_projects = (Project*) malloc(NUM_PROJECTS * sizeof(Project));
-	cudaMalloc((void**) &d_projects, NUM_PROJECTS * sizeof(Project));
-	cudaDeviceSynchronize();
-	checkForCudaErrors();
-
-	FILE *fp = fopen("cocomo.dataset", "r");
-	if (fp == NULL) {
-		printf("ERROR: Unable to open dataset file.");
-		return 1;
+void print(HighlyPrecise genes[], Project projects[]) {
+	printf("ProjectId\tKloc\tActualEffort\tEstimatedEffortGA\n");
+	for (int i = 0; i < NUM_PROJECTS; i++) {
+		HighlyPrecise estimatedEffort = genes[0]
+				* pow(projects[i].kloc, genes[1]);
+		printf("%d\t%lf\t%lf\t%lf\n", i, projects[i].kloc,
+				projects[i].actualEffort, estimatedEffort);
 	}
-	printf("==> Training data:\nKLOC\t\tActualEffort\n");
-	for (int i = 0; i < NUM_PROJECTS && !feof(fp); i++) {
-		fscanf(fp, "%Lf %Lf", h_projects[i].kloc, h_projects[i].actualEffort);
-		printf("%Lf\t%Lf\n", h_projects[i].kloc, h_projects[i].actualEffort);
-	}
-	fclose(fp);
-	cudaMemcpy(d_projects, h_projects, sizeof(Project) * NUM_PROJECTS,
-			cudaMemcpyHostToDevice);
-	cudaDeviceSynchronize();
-	checkForCudaErrors();
-	printf("Input dataset has been copied to GPU successfully.");
-	return d_projects;
+	printf("==> Parameters: ");
+	cout << "A: " << genes[0] << "\n";
+	cout << "B: " << genes[1] << "\n";
 }
 
-int main() {
-	int NUM_TOTAL_THREADS = NUM_BLOCKS * THREADS_PER_BLOCK;
-
-	// Setup the random number generation stream on device.
-	curandState *d_randomStates = NULL;
-	cudaMalloc((void**) &d_randomStates,
-			NUM_TOTAL_THREADS * sizeof(curandState));
-	setupRandomStream<<<NUM_BLOCKS, THREADS_PER_BLOCK>>>(time(NULL),
-			d_randomStates);
-	cudaDeviceSynchronize();
-	checkForCudaErrors();
-
-	Projects *d_dataset = loadDatasetIntoGPU();
-
-	Chromosome *h_gpuOut = NULL;
-	Chromosome *d_outputPopulation = NULL;
-	h_gpuOut = (Chromosome*) malloc(NUM_BLOCKS * sizeof(Chromosome));
-	cudaMalloc((void**) &d_outputPopulation, NUM_BLOCKS * sizeof(Chromosome));
-	cudaDeviceSynchronize();
-	checkForCudaErrors();
-
-	geneticAlgorithm<<<NUM_BLOCKS, THREADS_PER_BLOCK>>>(true, NULL,
-			d_randomStates, d_dataset, d_outputPopulation);
-
-	cudaMemcpy(h_gpuOut, d_outputPopulation, sizeof(Chromosome) * NUM_BLOCKS,
-			cudaMemcpyDeviceToHost);
-
-	//	stage2
-	Chromosome *d_inputPopulation;
-	cudaMalloc((void**) &d_inputPopulation, sizeof(Chromosome) * NUM_BLOCKS);
-	cudaDeviceSynchronize();
-	checkForCudaErrors();
-
-	for (int i = 0; i < NUM_BLOCKS; i++) {
-//		printf("block %d output:%lf\n", i, h_gpuOut[i].fitnessValue);
-	}
-	cudaMemcpy(d_inputPopulation, h_gpuOut, sizeof(Chromosome) * NUM_BLOCKS,
-			cudaMemcpyHostToDevice);
-
-	geneticAlgorithm<<<1, NUM_BLOCKS>>>(false, d_inputPopulation,
-			d_randomStates, d_dataset, d_outputPopulation);
-
-	cudaMemcpy(h_gpuOut, d_outputPopulation, sizeof(Chromosome) * 1,
-			cudaMemcpyDeviceToHost);
-	printf("======== GPU Results ========\n");
-	printf("==> Best Fitness Value: %e\n\nBest Chromosome: ",
-			h_gpuOut[0].fitnessValue);
-	for (int i = 0; i < GENOME_LENGTH; i++) {
-		printf("%e ", h_gpuOut[0].genes[i]);
-	}
-	printf("\n");
-
-	cudaDeviceSynchronize();
-	checkForCudaErrors();
-
-	// Freeing the resources.
-	cudaFree(d_randomStates);
-	cudaFree(d_outputPopulation);
-	return 0;
-}
